3d0e628c16174829beaa777d3a6038a11965d728
khanhlq <khanh96le@gmail.com>
2017-08-29 22:50:46 +0700

    Reorder project folders

--- src/find_the_differences.py
+++ src/find_the_differences.py
@@ -1,5 +1,9 @@
+"""
+Find the different between 2 images by subtracting 2 images
+"""
+
+
 import cv2
-import numpy as np
 
 # Read and convert to gray image
 left = cv2.imread('../images/right_2.jpg')
@@ -9,6 +13,7 @@ left = cv2.cvtColor(left, cv2.COLOR_RGB2GRAY)
 right = cv2.imread('../images/right.jpg')
 right = cv2.cvtColor(right, cv2.COLOR_RGB2GRAY)
 
+# Get the subtraction
 sub_matrix = left -right
 cv2.imshow('The differences', sub_matrix)
 cv2.waitKey()
