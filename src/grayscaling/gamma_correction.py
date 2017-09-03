6f9208880721e4d48e621a5550221b2d93b1f6f9
khanhlq <khanh96le@gmail.com>
2017-09-03 20:14:40 +0700

    Gamma correction

--- src/grayscaling/gamma_correction.py
+++ src/grayscaling/gamma_correction.py
@@ -1,15 +1,14 @@
 """
-Intensive transformation is an basic gray level transformation
+Gamma correction is an basic gray level transformation
 Formula:
 
-    X = ( 255 / (high - low) ) * ( X - low)
+    X = 255 - (X / 255) ^ y
 
     X: The intensive in each pixel
     255: The max level intensive of image
-    high: The highest gray level of image
-    low: The lowest gray level of image
-
+    y: Gamma level
 """
+
 
 import cv2
 import numpy as np
@@ -20,19 +19,19 @@ image = cv2.imread('../../images/wiki.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 
-def contrast_schetching(image):
-    # Get the high and low level intensive
-    high = image.max()
-    low = image.min()
-
+def gamma_correction(image, gamma=1):
     # Copy gray image
     image_gray = np.copy(image)
 
     # Contrast stretching
     for pixel in np.nditer(image_gray, op_flags=['readwrite']):
-        pixel[...] = np.round(np.abs((255 / float(high - low)) * (pixel - low)))
+        pixel[...] = np.round(255 * np.power(pixel/float(255), gamma))
     return image_gray
 
 
-res = np.hstack((image, contrast_schetching(image)))
+res = np.hstack((
+    image,
+    gamma_correction(image, gamma=2),
+    gamma_correction(image, gamma=0.5)
+))
 show_image(res)
