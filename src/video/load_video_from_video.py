1e378defd12c5db79dbc61ec8e774d178d2e4d6b
khanh96le <khanh96le@gmail.com>
2018-01-16 22:22:26 +0700

    Update opencv from anaconda list back to pip opencv-python

--- src/video/load_video_from_video.py
+++ src/video/load_video_from_video.py
@@ -1,15 +1,17 @@
 import cv2
+import os
 import numpy as np
 
-cap = cv2.VideoCapture('/home/khanhlq/Pictures/7s.mp4')
+CWD = os.getcwd()
+
+cap = cv2.VideoCapture('../../asserts/videos/MVI_1058.avi')
 
 while cap.isOpened():
     ret, frame = cap.read()
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 cap.release()
