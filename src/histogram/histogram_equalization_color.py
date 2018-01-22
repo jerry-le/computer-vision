a3574301aea01c8073903f1ef428533bbb894b33
khanh96le <khanh96le@gmail.com>
2018-01-22 10:18:51 +0700

    Add main to histogram color image

--- src/histogram/histogram_equalization_color.py
+++ src/histogram/histogram_equalization_color.py
@@ -3,16 +3,21 @@ import numpy as np
 from __utils__.general import show_image
 
 
-image = cv2.imread('../../asserts/images/nguocsang.jpg')
-image_y_cr_cb = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+def equalizeHistColor(img):
+    # Convert BGR image to YUV
+    image_y_cr_cb = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
 
-# Equalize the histogram of Y channel
-image_y_cr_cb[:, :, 0] = cv2.equalizeHist(image_y_cr_cb[:, :, 0])
+    # Equalize the histogram of Y channel
+    image_y_cr_cb[:, :, 0] = cv2.equalizeHist(image_y_cr_cb[:, :, 0])
+
+    # Convert the YUV back to RGB
+    output = cv2.cvtColor(image_y_cr_cb, cv2.COLOR_YUV2RGB)
+
+    return output
 
 
-# Convert the YCRCB back to RGB
-output = cv2.cvtColor(image_y_cr_cb, cv2.COLOR_YUV2RGB)
-
-res = np.hstack((image, output))
-show_image(res)
-
+if __name__ == '__main__':
+    image = cv2.imread('../../asserts/images/nguocsang.jpg')
+    output = equalizeHistColor(image)
+    res = np.hstack((image, output))
+    show_image(res)
